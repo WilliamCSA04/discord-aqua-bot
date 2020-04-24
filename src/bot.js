346837198ee4ca14af5fc82311946ba3683ab7b0
Williamcsa04 <williamcaetano14@gmail.com>
2020-04-24 07:35:54 -0300

    :sparkles: Added configurable separator

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const dotenv = require("dotenv");
 dotenv.config();
+
 const prefix = process.env.PREFIX;
+const separator = process.env.SEPARATOR;
 
 client.once("ready", () => {
   console.log("ready");
@@ -11,15 +13,16 @@ client.once("ready", () => {
 client.on("message", (message) => {
   if (!message.content.includes(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(" ");
+  const args = message.content.slice(prefix.length).split(separator);
   const command = args.shift().toLowerCase();
   if (command === "water") {
     message.channel.send(
       "Okay, I will remind you to drink water every half an hour"
     );
+    const timer = 1800000;
     setInterval(() => {
       message.channel.send("Drink water!");
-    }, 1800000);
+    }, timer);
   }
 });
 
