eba5f286427c50a0334ef7941ff2dd5fbf53e3a5
Williamcsa04 <williamcaetano14@gmail.com>
2020-04-29 22:33:04 -0300

    :sparkles: Bot now accepts one argument with start

--- src/Bot/Commands.js
+++ src/Bot/Commands.js
@@ -7,12 +7,13 @@ export function start(message) {
   if (!message.content.includes(prefix) || message.author.bot) return;
   const command = commandHandler(message);
   if (command.main === "start") {
+    const timeInMinutes = command.args[0] || 20;
+    const timerInMiliseconds = timeInMinutes * 60 * 1000;
     message.channel.send(
-      "Okay, I will remind you to drink water every 20 minutes"
+      `Okay, I will remind you to drink water every ${timeInMinutes} minutes`
     );
-    const timer = 1200000;
     setInterval(() => {
       message.channel.send("Drink water!");
-    }, timer);
+    }, timerInMiliseconds);
   }
 }
