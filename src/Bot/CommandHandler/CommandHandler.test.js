b78ac277e8ad31716453b8be0852968729d4815f
Williamcsa04 <williamcaetano14@gmail.com>
2020-09-12 03:19:30 -0300

    :white_check_mark: Created test for command with args

--- src/Bot/CommandHandler/CommandHandler.test.js
+++ src/Bot/CommandHandler/CommandHandler.test.js
@@ -1,4 +1,5 @@
 import commandHandler from "./CommandHandler";
+import config from "../../config";
 
 describe("Command handler tests", () => {
   test("Expect that CommandHandler without message return an error", () => {
@@ -8,5 +9,12 @@ describe("Command handler tests", () => {
     const response = commandHandler({ content: "@Aqua start 10" });
     expect(response).toHaveProperty("main");
     expect(response).toHaveProperty("args");
+  });
+  test("Expect that CommandHandler return a json with main with start and args with ['10']", () => {
+    const { main, args } = commandHandler({
+      content: `${config.prefix}start 10`,
+    });
+    expect(main).toBe("start");
+    expect(args).toEqual(["10"]);
   });
 });
